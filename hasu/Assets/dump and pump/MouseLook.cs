00d1758187e459d406f79bb428b61407b6e35b99
asecoder3 <54946934+asecoder3@users.noreply.github.com>
2021-10-28 19:27:23 +0300

    mouse look valmis

--- hasu/Assets/dump and pump/MouseLook.cs
+++ hasu/Assets/dump and pump/MouseLook.cs
@@ -17,12 +17,23 @@ public class MouseLook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+      Cursor.lockState =mode;
+      playerbody = gameObject.transform.parent;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         mouseX = Input.GetAxis("Mouse X") *mouseSensitivy * Time.deltaTime;
+         mouseY = Input.GetAxis("Mouse Y") *mouseSensitivy * Time.deltaTime;   
+         xRotation -= mouseY;
+
+         xRotation = Mathf.Clamp(xRotation,minXAngle,maxAngle);
+         transform.localRotation = Quaternion.Euler(xRotation,0 ,0);
+         playerbody.Rotate(Vector3.up * mouseX);
+         
     }
+         
+
+
 }
